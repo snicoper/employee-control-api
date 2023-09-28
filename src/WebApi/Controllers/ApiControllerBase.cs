d11ee35f9f850fe64795876cd26f59fa3ab71e1c
Salvador Nicolas <snicoper@gmail.com>
2023-09-28 23:11:27 +0200

    Added ApiControllerBase

---
+++ src/WebApi/Controllers/ApiControllerBase.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeControl.WebApi.Controllers;
+
+[ApiController]
+[Produces("application/json")]
+public class ApiControllerBase : ControllerBase
+{
+}
--- src/WebApi/Controllers/WeatherForecastController.cs
+++ src/WebApi/Controllers/WeatherForecastController.cs
@@ -2,24 +2,23 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace EmployeeControl.WebApi.Controllers;
 
-[ApiController]
 [Route("[controller]")]
-public class WeatherForecastController : ControllerBase
+public class WeatherForecastController : ApiControllerBase
 {
-    private static readonly string[] Summaries = new[]
+    private static readonly string[] Summaries =
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching",
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)],
-        })
-        .ToArray();
+            {
+                Date = DateOnly.FromDateTime(DateTime.UtcNow.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
     }
 }
