979dfe650c5b44f7f65bf72ed7ecb9056bf40294
Salvador Nicolas <snicoper@gmail.com>
2023-09-29 09:24:36 +0200

    Added Extensions in Application

---
+++ src/Application/Common/Extensions/EnumExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeControl.Application.Common.Extensions;
+
+public static class EnumExtensions
+{
+    /// <summary>
+    ///     Obtiene el valor de [Display(Name = "")] en un Enum.
+    /// </summary>
+    public static string EnumDisplayNameFor(this Enum value)
+    {
+        var enumType = value.GetType();
+        var enumValue = Enum.GetName(enumType, value);
+
+        if (enumValue is null)
+        {
+            return string.Empty;
+        }
+
+        var member = enumType.GetMember(enumValue)[0];
+        var attrs = member.GetCustomAttributes(typeof(DisplayAttribute), false);
+        var outString = ((DisplayAttribute)attrs[0]).Name;
+
+        if (((DisplayAttribute)attrs[0]).ResourceType is not null)
+        {
+            outString = ((DisplayAttribute)attrs[0]).GetName();
+        }
+
+        return outString ?? string.Empty;
+    }
+}
---
+++ src/Application/Common/Extensions/StringExtensions.cs
@@ -0,0 +1,32 @@
+﻿namespace EmployeeControl.Application.Common.Extensions;
+
+public static class StringExtensions
+{
+    /// <summary>
+    ///     Devolver un string con la primera letra en minúscula.
+    /// </summary>
+    public static string LowerCaseFirst(this string str)
+    {
+        return string.IsNullOrEmpty(str) ? str : $"{str[..1].ToLower()}{str[1..]}";
+    }
+
+    /// <summary>
+    ///     Devolver un string con la primera letra en mayúscula.
+    /// </summary>
+    public static string UpperCaseFirst(this string str)
+    {
+        return string.IsNullOrEmpty(str) ? str : $"{str[..1].ToUpper()}{str[1..]}";
+    }
+
+    /// <summary>
+    ///     Devolver un string con la primera letra de cada palabra en mayúscula.
+    /// </summary>
+    public static string ToTile(this string str)
+    {
+        var parts = str.Split()
+            .Select(part => part.UpperCaseFirst())
+            .ToList();
+
+        return string.IsNullOrEmpty(str) ? str : string.Join(" ", parts);
+    }
+}
