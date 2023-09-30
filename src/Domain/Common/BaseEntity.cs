7c494e0881e0e6aa6223f7e3eb7941f0ef252709
Salvador Nicolas <snicoper@gmail.com>
2023-09-30 09:18:08 +0200

    Fix code

--- src/Domain/Common/BaseEntity.cs
+++ src/Domain/Common/BaseEntity.cs
@@ -6,11 +6,10 @@ public abstract class BaseEntity
 {
     private readonly List<BaseEvent> _domainEvents = new();
 
-    // This can easily be modified to be BaseEntity<T> and public T Id to support different key types.
-    // Using non-generic integer types for simplicity.
     public int Id { get; set; }
 
-    [NotMapped] public IReadOnlyCollection<BaseEvent> DomainEvents => _domainEvents.AsReadOnly();
+    [NotMapped]
+    public IReadOnlyCollection<BaseEvent> DomainEvents => _domainEvents.AsReadOnly();
 
     public void AddDomainEvent(BaseEvent domainEvent)
     {
