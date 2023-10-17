0eaacc4b4bd09274290e663a2d13f08e97bbc41e
Salvador Nicolas <snicoper@gmail.com>
2023-10-17 04:27:37 +0200

    Cambiado orden propiedades

--- src/Application/Common/Constants/AppCultures.cs
+++ src/Application/Common/Constants/AppCultures.cs
@@ -4,18 +4,18 @@ namespace EmployeeControl.Application.Common.Constants;
 
 public static class AppCultures
 {
-    public static readonly CultureInfo Es = new("es");
-
-    public static readonly CultureInfo EsEs = new("es-ES");
-
-    public static readonly CultureInfo En = new("en");
-
-    public static readonly CultureInfo EnUs = new("en-US");
-
     /// <summary>
     /// Cultura por defecto.
     /// </summary>
-    public static readonly CultureInfo DefaultCulture = EsEs;
+    public static readonly CultureInfo DefaultCulture = new("es-ES");
+
+    private static readonly CultureInfo Es = new("es");
+
+    private static readonly CultureInfo EsEs = new("es-ES");
+
+    private static readonly CultureInfo En = new("en");
+
+    private static readonly CultureInfo EnUs = new("en-US");
 
     /// <summary>
     /// Obtener todas las culturas disponibles.
