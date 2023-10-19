d28d7d05bd112ca2c48556c438803dc121f78494
Salvador Nicolas <snicoper@gmail.com>
2023-10-19 16:14:44 +0200

    Datos de prueba en GetAdminIdentitiesPaginatedResponse mas coherentes

--- src/Application/Features/Admin/AdminIdentity/Queries/GetAdminIdentitiesPaginated/GetAdminIdentitiesPaginatedResponse.cs
+++ src/Application/Features/Admin/AdminIdentity/Queries/GetAdminIdentitiesPaginated/GetAdminIdentitiesPaginatedResponse.cs
@@ -8,14 +8,15 @@ public record GetAdminIdentitiesPaginatedResponse : IMapFrom<ApplicationUser>
 {
     public string? Id { get; set; }
 
-    public string? NombreDeUsuario { get; set; }
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set; }
+
+    public string? Email { get; set; }
 
     public void Mapping(Profile profile)
     {
         profile
-            .CreateMap<ApplicationUser, GetAdminIdentitiesPaginatedResponse>()
-            .ForMember(
-                dest => dest.NombreDeUsuario,
-                opt => opt.MapFrom(src => $"{src.UserName}, {src.UserName}"));
+            .CreateMap<ApplicationUser, GetAdminIdentitiesPaginatedResponse>();
     }
 }
