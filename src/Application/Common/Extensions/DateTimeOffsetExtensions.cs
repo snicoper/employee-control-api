2ee49ccdc9cccf507f6db16167fa5dd7bec68171
Salvador Nicolas <snicoper@gmail.com>
2023-11-02 19:34:38 +0100

    Rev DateTimeOffsetExtensions

--- src/Application/Common/Extensions/DateTimeOffsetExtensions.cs
+++ src/Application/Common/Extensions/DateTimeOffsetExtensions.cs
@@ -10,7 +10,7 @@ public static class DateTimeOffsetExtensions
     /// <returns>Un <see cref="DateTimeOffset" /> con la hora final del día.</returns>
     public static DateTimeOffset EndOfDay(this DateTimeOffset dateTimeOffset, TimeProvider timeProvider)
     {
-        var offset = timeProvider.LocalTimeZone.GetUtcOffset(DateTime.UtcNow).TotalMinutes;
+        var offset = timeProvider.LocalTimeZone.GetUtcOffset(DateTimeOffset.UtcNow).TotalMinutes;
         var datetime = dateTimeOffset.Date.AddHours(23).AddMinutes(59).AddMinutes(offset * -1).AddSeconds(59);
         var result = new DateTimeOffset(datetime, TimeSpan.Zero);
 
@@ -25,7 +25,7 @@ public static class DateTimeOffsetExtensions
     /// <returns>Un <see cref="DateTimeOffset" /> con la hora inicial del día.</returns>
     public static DateTimeOffset StartOfDay(this DateTimeOffset dateTimeOffset, TimeProvider timeProvider)
     {
-        var offset = timeProvider.LocalTimeZone.GetUtcOffset(DateTime.UtcNow).TotalMinutes;
+        var offset = timeProvider.LocalTimeZone.GetUtcOffset(DateTimeOffset.UtcNow).TotalMinutes;
         var datetime = dateTimeOffset.Date.AddHours(00).AddMinutes(00).AddMinutes(offset * -1).AddSeconds(00);
         var result = new DateTimeOffset(datetime, TimeSpan.Zero);
 
