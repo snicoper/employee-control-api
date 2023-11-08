25a4cd035a3cd7528f3a07b0ba4b920e373ceb67
Salvador Nicolas <snicoper@gmail.com>
2023-11-08 09:04:55 +0100

    ValueObject GetEqualityComponents permite object mullable

--- src/Domain/Common/ValueObject.cs
+++ src/Domain/Common/ValueObject.cs
@@ -18,7 +18,7 @@ public abstract class ValueObject
     public override int GetHashCode()
     {
         return GetEqualityComponents()
-            .Select(x => x.GetHashCode())
+            .Select(x => x != null ? x.GetHashCode() : 0)
             .Aggregate((x, y) => x ^ y);
     }
 
@@ -37,7 +37,7 @@ public abstract class ValueObject
         return !EqualOperator(left, right);
     }
 
-    protected abstract IEnumerable<object> GetEqualityComponents();
+    protected abstract IEnumerable<object?> GetEqualityComponents();
 
     // Other utility methods
 }
