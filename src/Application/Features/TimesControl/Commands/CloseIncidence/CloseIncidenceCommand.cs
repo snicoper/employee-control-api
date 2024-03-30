ec72505acefcebe87148b5b8924daa34812e0a08
Salvador Nicolas <snicoper@gmail.com>
2024-03-30 10:37:29 +0100

    Anadido authorization en CloseIncidenceCommand

--- src/Application/Features/TimesControl/Commands/CloseIncidence/CloseIncidenceCommand.cs
+++ src/Application/Features/TimesControl/Commands/CloseIncidence/CloseIncidenceCommand.cs
@@ -1,6 +1,9 @@
 ﻿using EmployeeControl.Application.Common.Models;
+using EmployeeControl.Application.Common.Security;
+using EmployeeControl.Domain.Constants;
 using MediatR;
 
 namespace EmployeeControl.Application.Features.TimesControl.Commands.CloseIncidence;
 
+[Authorize(Roles = Roles.HumanResources)]
 public record CloseIncidenceCommand(string Id) : IRequest<Result>;
