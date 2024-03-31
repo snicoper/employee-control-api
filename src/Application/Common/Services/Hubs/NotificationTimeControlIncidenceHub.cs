73eaf37f7339196a71154f110a49fdf88303cd13
Salvador Nicolas <snicoper@gmail.com>
2024-03-31 10:45:48 +0200

    Anadido log Microsoft.AspNetCore.SignalR

--- src/Application/Common/Services/Hubs/NotificationTimeControlIncidenceHub.cs
+++ src/Application/Common/Services/Hubs/NotificationTimeControlIncidenceHub.cs
@@ -1,7 +1,15 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using EmployeeControl.Application.Common.Constants;
+using Microsoft.AspNetCore.SignalR;
 
 namespace EmployeeControl.Application.Common.Services.Hubs;
 
 public class NotificationTimeControlIncidenceHub : Hub
 {
+    /// <summary>
+    /// Notifica que se ha añadido o eliminado una incidencia.
+    /// </summary>
+    public async Task Notify()
+    {
+        await Clients.All.SendAsync(HubNames.TimeControlIncidences);
+    }
 }
