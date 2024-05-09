ad99f747c8d47571530743a51362a3829cd92800
Salvador Nicolas <snicoper@gmail.com>
2024-05-09 21:49:43 +0200

    Anadido comentarios a los metodos

--- src/Application/Common/Models/Result.cs
+++ src/Application/Common/Models/Result.cs
@@ -19,46 +19,94 @@ public class Result
 
     public ICollection<ValidationFailure> Errors { get; }
 
+    /// <summary>
+    /// Crea una nueva instancia de <see cref="Result" /> con los valores predeterminados.
+    /// </summary>
+    /// <returns>Instancia de <see cref="Result" />.</returns>
     public static Result Create()
     {
         return new Result(false, []);
     }
 
+    /// <summary>
+    /// Crea una nueva instancia de <see cref="Result{TValue}" /> con los valores predeterminados.
+    /// </summary>
+    /// <param name="value">Valor de tipo de resultado.</param>
+    /// <typeparam name="TValue">Tipo esperado en el Result.</typeparam>
+    /// <returns>Instancia de <see cref="Result{TValue}" />.</returns>
     public static Result<TValue> Create<TValue>(TValue? value)
     {
         return new Result<TValue>(value, true, []);
     }
 
+    /// <summary>
+    /// Crea una nueva instancia de <see cref="Result" /> sin errores.
+    /// </summary>
+    /// <returns>Instancia de <see cref="Result" />.</returns>
     public static Result Success()
     {
         return new Result(true, []);
     }
 
+    /// <summary>
+    /// Crea una nueva instancia de <see cref="Result{TValue}" /> sin errores.
+    /// </summary>
+    /// <param name="value">Valor de tipo de resultado.</param>
+    /// <typeparam name="TValue">Tipo esperado en el Result.</typeparam>
+    /// <returns>Instancia de <see cref="Result{TValue}" />.</returns>
     public static Result<TValue> Success<TValue>(TValue? value)
     {
         return new Result<TValue>(value, true, []);
     }
 
+    /// <summary>
+    /// Crea una nueva instancia de <see cref="Result" /> con una colección de <see cref="ValidationFailure" />.
+    /// </summary>
+    /// <param name="errors">Lista de <see cref="ValidationFailure" />.</param>
+    /// <returns>El <see cref="Result" /> con el error añadido.</returns>
     public static Result Failure(ICollection<ValidationFailure> errors)
     {
         return new Result(false, errors);
     }
 
+    /// <summary>
+    /// Crea una nueva instancia de <see cref="Result{TValue}" /> con una colección de <see cref="ValidationFailure" />.
+    /// </summary>
+    /// <param name="errors">Lista de <see cref="ValidationFailure" />.</param>
+    /// <typeparam name="TValue">Tipo esperado en el Result.</typeparam>
+    /// <returns>El <see cref="Result{TValue}" /> con el error añadido.</returns>
     public static Result<TValue> Failure<TValue>(ICollection<ValidationFailure> errors)
     {
         return new Result<TValue>(default, false, errors);
     }
 
+    /// <summary>
+    /// Crea una nueva instancia de <see cref="Result" /> con un error <see cref="ValidationFailure" />.
+    /// </summary>
+    /// <param name="validationFailure">Error a insertar.</param>
+    /// <returns>El <see cref="Result" /> con el error añadido.</returns>
     public static Result Failure(ValidationFailure validationFailure)
     {
         return Failure([validationFailure]);
     }
 
+    /// <summary>
+    /// Crea una nueva instancia de <see cref="Result{TValue}" /> con un error <see cref="ValidationFailure" />.
+    /// </summary>
+    /// <param name="validationFailure">Error a insertar.</param>
+    /// <typeparam name="TValue">Tipo esperado en el Result.</typeparam>
+    /// <returns>El <see cref="Result{TValue}" /> con el error añadido.</returns>
     public static Result<TValue> Failure<TValue>(ValidationFailure validationFailure)
     {
         return Failure<TValue>([validationFailure]);
     }
 
+    /// <summary>
+    /// Crea una nueva instancia de <see cref="Result" /> con un error.
+    /// </summary>
+    /// <param name="code">Código del error.</param>
+    /// <param name="error">Error a insertar.</param>
+    /// <returns>El <see cref="Result" /> con el error añadido.</returns>
     public static Result Failure(string code, string error)
     {
         var validationFailure = new ValidationFailure(code, error);
@@ -66,6 +114,13 @@ public class Result
         return Failure(validationFailure);
     }
 
+    /// <summary>
+    /// Crea una nueva instancia de <see cref="Result{TValue}" /> con un error.
+    /// </summary>
+    /// <param name="code">Código del error.</param>
+    /// <param name="error">Error a insertar.</param>
+    /// <typeparam name="TValue">Tipo esperado en el Result.</typeparam>
+    /// <returns>El <see cref="Result{TValue}" /> con el error añadido.</returns>
     public static Result<TValue> Failure<TValue>(string code, string error)
     {
         var validationFailure = new ValidationFailure(code, error);
@@ -73,6 +128,10 @@ public class Result
         return Failure<TValue>(validationFailure);
     }
 
+    /// <summary>
+    /// Añade un nuevo error.
+    /// </summary>
+    /// <param name="validationFailure"><see cref="ValidationFailure" />.</param>
     public void AddValidationFailure(ValidationFailure validationFailure)
     {
         Succeeded = false;
@@ -81,11 +140,20 @@ public class Result
         Errors.Add(validationFailure);
     }
 
+    /// <summary>
+    /// Añade un nuevo error.
+    /// </summary>
+    /// <param name="code">Código del error.</param>
+    /// <param name="error">Error a insertar.</param>
     public void Add(string code, string error)
     {
         AddValidationFailure(new ValidationFailure(code, error));
     }
 
+    /// <summary>
+    /// Lanza un <see cref="CustomValidationException" /> si existen errores.
+    /// </summary>
+    /// <exception cref="CustomValidationException"><see cref="CustomValidationException" />.</exception>
     public void RaiseBadRequestIfResultFailure()
     {
         if (Succeeded)
@@ -93,6 +161,15 @@ public class Result
             return;
         }
 
+        RaiseBadRequest();
+    }
+
+    /// <summary>
+    /// Lanza un <see cref="CustomValidationException" /> con los errores producidos.
+    /// </summary>
+    /// <exception cref="CustomValidationException"><see cref="CustomValidationException" />.</exception>
+    public void RaiseBadRequest()
+    {
         throw new CustomValidationException([.. Errors]);
     }
 }
